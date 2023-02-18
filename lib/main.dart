1d6be172f8ed5a4cbd75e2da528d8736b64d4aff
Varghese <shelvinvarghese6@gmail.com>
2023-02-18 11:08:09 +0530

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,9 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
-          appBarTheme: const AppBarTheme(backgroundColor: Colors.transparent),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.transparent,
+          ),
           primarySwatch: Colors.blue,
           backgroundColor: Colors.black,
           scaffoldBackgroundColor: backgroundColor,
